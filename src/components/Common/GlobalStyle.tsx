d69efa9bb5e454822d38cd8ef1cd822cb3991efc
Ah RA <ooz43875@gmail.com>
2024-02-21 16:05:35 +0900

    style(GlobalStyle): change font for better readability

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -2,13 +2,19 @@ import React, { FunctionComponent } from 'react'
 import { Global, css } from '@emotion/react'
 
 const defaultStyle = css`
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap');
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
 
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Nanum Myeongjo', serif;
+    font-family: 'Pretendard-Regular';
   }
 
   html,
