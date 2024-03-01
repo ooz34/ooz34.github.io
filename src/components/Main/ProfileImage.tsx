cd147c0ca1dfc897d715942ab547b01e54cedebc
Ah RA <ooz43875@gmail.com>
2024-03-01 17:20:13 +0900

    chore: resize profile image

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,12 +1,12 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
-import { IGatsbyImageData } from 'gatsby-plugin-image'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
 
 type ProfileImageProps = {
   profileImage: IGatsbyImageData
 }
 
-const ProfileImageWrapper = styled.img`
+const ProfileImageWrapper = styled(GatsbyImage)`
   width: 120px;
   height: 120px;
   margin-bottom: 30px;
