ad77090168ec8f0f23a566d395ffaed078543119
Ah RA <ooz43875@gmail.com>
2024-03-01 21:29:13 +0900

    feat: add infinite scroll

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useMemo } from 'react'
 import styled from '@emotion/styled'
 import PostItem from './PostItem'
 import { PostListItemType } from 'types/PostItem.types'
+import useInfiniteScroll from 'hooks/useInfiniteScroll'
 
 export type PostType = {
   node: {
@@ -42,24 +43,11 @@ const PostList: FunctionComponent<PostListProps> = function ({
   selectedCategory,
   posts,
 }) {
-  const postListData = useMemo(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: PostListItemType) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
-  )
+  const { containerRef, postList } = useInfiniteScroll(selectedCategory, posts)
 
   return (
-    <PostListWrapper>
-      {postListData.map(({ node: { id, frontmatter } }: PostListItemType) => (
+    <PostListWrapper ref={containerRef}>
+      {postList.map(({ node: { id, frontmatter } }: PostListItemType) => (
         <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
       ))}
     </PostListWrapper>
