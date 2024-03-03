1ab417ba96d12dd8e343a39894d5247ba0042ad3
Ah RA <ooz43875@gmail.com>
2024-03-04 02:31:56 +0900

    Updates

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Text from 'components/Text';
-import React, { FunctionComponent } from 'react';
-
-const IndexPage: FunctionComponent = function () {
-    return <Text text="Home"/>
-}
-
-export default IndexPage;
